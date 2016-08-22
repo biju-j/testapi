3e2fe663b562e0308802f6630de86afedf54bb2e
Biju Janardhanan <biju.janardhanan@blippar.com>
2016-08-22 17:53:01 +0530

    Added Create Marker and Other test suite

---
+++ Acceptance tests/users.js
@@ -0,0 +1,74 @@
+'use strict';
+var mocha = require('mocha');
+var SupportAPI = require('../lib/support.js');
+var logging      = require( '../config/config-log4js.js' );
+var fs = require('fs');
+var logger=logging.LOG;
+
+var chai   = require( 'chai' );
+var expect = chai.expect;
+var now = new Date();
+var supportapi = new SupportAPI();
+
+var bRole='blippar_admin';
+describe(' USERS Tests', function(){
+           this.timeout(200000);
+
+           it('All Users', function(done){
+                    supportapi.fetcher(bRole,'/users', function(text){
+                    // console.log("All users > "+text);
+                    expect(text).to.contain("Email", "Groups", "Roles", "Status", "Campaigns");
+                                  done();
+                    });
+           });
+
+
+
+           it('Permissions of a User', function(done){
+                   supportapi.fetcher(bRole,'/user/6411/permissions', function(text){
+                   // console.log("Permissions of user > "+text);
+                   expect(text).to.contain("Permission", "Caller", "Action");
+                   done();
+                });
+           });
+
+           it('Fetch Current User', function(done){
+                  supportapi.fetcher(bRole,'/user', function(text){
+                  // console.log("Current user > "+text);
+                  expect(text).to.contain("Email", "CreatedAt", "CreatedByUserId", "StatusId");
+                  done();
+                });
+           });
+
+           it('User check', function(done){
+                   supportapi.fetcher(bRole,'/user/6411', function(text){
+                   // console.log("Check user > "+text);
+                   expect(text).to.contain("Email", "CreatedAt", "CreatedByUserId", "StatusId");
+                   done();
+                 });
+           });
+
+           it('Existent Email check', function(done){
+                   supportapi.fetcher(bRole,'/email/exists/bijupja.na@gmail.com', function(text){
+                   expect(text.trim()).to.equal("true");
+                   done();
+                 });
+           });
+
+           it('Non-existent Email check', function(done){
+                      supportapi.fetcher(bRole,'/email/exists/simple@gmail.com', function(text){
+                      expect(text.trim()).to.equal("false");
+                      done();
+                  });
+           });
+
+
+           it('User Status', function(done){
+                   supportapi.fetcher(bRole,'/user/6411/status', function(text){
+                   // console.log("Existent USER Status > "+text);
+                   expect(text).to.contain("Id", "Name", "Description");
+                   done();
+                 });
+           });
+
+});
